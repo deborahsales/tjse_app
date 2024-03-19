962c8dbdfa4e3740ba6ee95504da6c454018a83d
Déborah <dsales15@gmail.com>
2024-03-19 16:44:53 -0300

    integrando o projeto ao firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:disp_moveis/screens/autenticacao.dart';
-import 'screens/initial_screen.dart';
+import 'package:disp_moveis/screens/initial_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
